661f156ff96ee03019595bf3e1e5b2763c650233
Restu Haqqi Muzakir <restuhaqza@gmail.com>
2019-10-27 11:22:35 +0700

    add main method createSign and verifySign

---
+++ index.js
@@ -0,0 +1,45 @@
+var crypto = require("crypto")
+
+var private_key
+var public_key
+
+function setPrivateKey(value) {
+  private_key = value
+}
+
+function getPrivateKey(value) {
+  return private_key
+}
+
+function setPublicKey(value) {
+  public_key = value
+}
+
+function getPublicKey(value) {
+  return public_key
+}
+
+function createSign(message) {
+  const signer = crypto.createSign("sha256")
+  signer.update(message)
+  signer.end()
+
+  const signature = signer.sign(getPrivateKey(), "base64")
+  return signature
+}
+
+function verifySign(message, signature) {
+  const verifier = crypto.createVerify("sha256")
+  verifier.update(message)
+  verifier.end()
+
+  const verified = verifier.verify(getPublicKey(), signature, "base64")
+  return verified
+}
+
+module.exports = {
+  setPrivateKey,
+  setPublicKey,
+  createSign,
+  verifySign
+}
