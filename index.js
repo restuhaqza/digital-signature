44e44da5590b97f6195645e6be85d4bea4f38746
testasdsdas <kassissahil@gmail.com>
2021-05-11 21:20:27 +0400

    Added comments

--- index.js
+++ index.js
@@ -6,22 +6,43 @@ class Digital_Signature{
     this.public_key;
   }
 
-  setPrivateKey(value) {
-    this.private_key = value;
+  /**
+   * Sets private key
+   * @param {string} privateKey - Value to set the private key to.
+  */
+  setPrivateKey(privateKey) {
+    this.private_key = privateKey;
   }
 
+  /**
+  * Returns private key
+  * @returns {string} private_key
+  */
   getPrivateKey() {
     return this.private_key;
   }
 
-  setPublicKey(value) {
-    this.public_key = value;
+  /**
+   * Sets public key
+   * @param {string} publicKey - Value to set the public key to.
+  */
+  setPublicKey(publicKey) {
+    this.public_key = publicKey;
   }
 
+  /**
+  * Returns private key
+  * @returns {string} publick_key 
+  */
   getPublicKey() {
     return this.public_key;
   }
 
+  /**
+   * Generates a new pair of RSA keys
+   * Automatically assigns generated keys to constructor key variables.
+   * @param {function} _callback - Optional callback function. Takes err, publicKey, privateKey as parameters. 
+  */
   async generateNewKeyPair(_callback = null){
     await crypto.generateKeyPair('rsa', {
       modulusLength: 1024,
@@ -41,6 +62,11 @@ class Digital_Signature{
     });
   }
 
+  /**
+   * Signs a message using the private key. 
+   * @param {string} message - String to sign with publc key. 
+   * @returns {string} signature - Signed message. 
+  */
   createSign(message) {
     const signer = crypto.createSign("sha256");
     signer.update(message);
@@ -50,6 +76,12 @@ class Digital_Signature{
     return signature;
   }
 
+  /**
+   * Verifies a signed message using the signer's public key. 
+   * @param {string} message - A signed message.
+   * @param {string} signature - The signature assosciated with the signed message. 
+   * @returns {boolean} verified - True/false
+  */
   verifySign(message, signature) {
     const verifier = crypto.createVerify("sha256");
     verifier.update(message);
