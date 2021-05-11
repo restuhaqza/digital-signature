48abcd4d5fc5f6218005572aa1459c417bef93fb
testasdsdas <kassissahil@gmail.com>
2021-05-11 19:52:06 +0400

    moved functions to a class; updated module.exports

--- index.js
+++ index.js
@@ -1,47 +1,44 @@
 var crypto = require("crypto")
 
-var private_key
-var public_key
+class Digital_Signature{
+  constructor(){
+    this.private_key;
+    this.public_key;
+  }
 
-function setPrivateKey(value) {
-  private_key = value
-}
+  setPrivateKey(value) {
+    this.private_key = value;
+  }
 
-function getPrivateKey(value) {
-  return private_key
-}
+  getPrivateKey() {
+    return this.private_key;
+  }
 
-function setPublicKey(value) {
-  public_key = value
-}
+  setPublicKey(value) {
+    this.public_key = value;
+  }
 
-function getPublicKey(value) {
-  return public_key
-}
+  getPublicKey() {
+    return this.public_key;
+  }
 
-function createSign(message) {
-  const signer = crypto.createSign("sha256")
-  signer.update(message)
-  signer.end()
+  createSign(message) {
+    const signer = crypto.createSign("sha256");
+    signer.update(message);
+    signer.end();
+  
+    const signature = signer.sign(this.getPrivateKey(), "base64");
+    return signature;
+  }
 
-  const signature = signer.sign(getPrivateKey(), "base64")
-  return signature
-}
+  verifySign(message, signature) {
+    const verifier = crypto.createVerify("sha256");
+    verifier.update(message);
+    verifier.end();
+  
+    const verified = verifier.verify(this.getPublicKey(), signature, "base64");
+    return verified;
+  }
+};
 
-function verifySign(message, signature) {
-  const verifier = crypto.createVerify("sha256")
-  verifier.update(message)
-  verifier.end()
-
-  const verified = verifier.verify(getPublicKey(), signature, "base64")
-  return verified
-}
-
-module.exports = {
-  setPrivateKey,
-  getPrivateKey,
-  setPublicKey,
-  getPublicKey,
-  createSign,
-  verifySign
-}
+module.exports = Digital_Signature;
